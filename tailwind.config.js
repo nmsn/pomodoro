80c8850e4f313ebdc5955b3f54c96832adf150cb
nmsn <136696700@qq.com>
2022-09-23 23:45:35 +0800

    feat: complete base pomodoro function

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
