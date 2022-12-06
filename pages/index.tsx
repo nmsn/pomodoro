daa8474c3f35222eac4a3b8ee3f921f41aad7443
136696700@qq.com <136696700@qq.com>
2022-12-06 18:00:25 +0800

    feat: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import Todo from "../components/Todo";
 import Clock, { openState } from "../components/Clock";
+import { useSsrComplectedState } from "../utils/hooks";
 
 const Home: NextPage = () => {
+  const setSsrCompleted = useSsrComplectedState();
+  useEffect(setSsrCompleted, [setSsrCompleted]);
+
   const open = useRecoilValue(openState);
   return (
     <div className="w-screen h-screen bg-blue-400 flex">
