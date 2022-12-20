711af298fc8adb7050c1d3f9959f389daa9e3127
136696700@qq.com <136696700@qq.com>
2022-12-20 10:10:12 +0800

    feat: update button props

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,15 +4,26 @@ const Button = ({
   children,
   onClick,
   className = "text-blue-400",
+  size = "normal",
+  type = "inverse",
 }: {
   children: string;
   onClick?: () => void;
   className?: string;
+  size?: "normal" | "small";
+  type?: "positive" | "inverse";
 }) => {
   return (
     <button
+      // className={classnames(size === "small" ? "btn-small" : "btn", className)}
       className={classnames(
-        "btn",
+        {
+          "btn-small": size === "small",
+          "btn-normal": size === "normal",
+          "btn-positive": type === "positive",
+          "btn-inverse": type === "inverse",
+          btn: true,
+        },
         className
       )}
       onClick={onClick}
