4b07e899aa836081fb17cbda047762150c34c9b1
136696700@qq.com <136696700@qq.com>
2023-04-08 18:39:24 +0800

    feat(utils): 设备识别

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+export const isMobile = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+};
+
+export const isIpad = () => {
+  return /iPad/i.test(navigator.userAgent);
+};
+
+export const browserHeaderHeight = () => {
+  const innerH = window.innerHeight; //W3C-不包括菜单栏、工具栏以及滚动条等的浏览器窗口高度
+  const outH = window.outerHeight; //整个浏览器窗口的高度
+
+  return outH - innerH;
+};
