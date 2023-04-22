3e49d3409a1bbcfed1bc1a9e54e73956ba28e2e4
136696700@qq.com <136696700@qq.com>
2023-04-22 09:58:22 +0800

    feat(components): add Clock

---
+++ src/components/Clock.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useMemo, useState } from 'react';
+import classnames from 'classnames';
+
+const Clock = ({ width = 'w-0', height = 'h-0' }: { width: string; height: string }) => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  const timeStr = useMemo(() => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return [hours, minutes, seconds].map(item => item.toString().padStart(2, '0')).join(':');
+  }, [date]);
+
+  return (
+    <div
+      className={classnames(
+        width,
+        height,
+        'flex justify-center items-center duration-300 bg-blue-400 relative overflow-hidden',
+      )}
+    >
+      <div className="w-120 flex-col block">
+        <div
+          className="text-9xl mt-6 mb-12 font-bold select-none"
+          style={{ fontVariantNumeric: 'tabular-nums' }}
+        >
+          {timeStr}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clock;
