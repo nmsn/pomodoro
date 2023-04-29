0bc92017c3adee85e9bce00e9ea5b52d326c0781
136696700@qq.com <136696700@qq.com>
2023-04-29 11:30:49 +0800

    feat: add github link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 import { isMobile } from '@/utils';
 
@@ -17,12 +18,15 @@ const Header = ({ open, onChange }: { open: boolean; onChange: (open: boolean) =
   };
 
   return (
-    <div className="absolute top-0 w-screen h-12 flex items-center px-4 space-x-4 z-10">
+    <div className="absolute top-0 w-screen h-12 flex justify-between items-center px-4 space-x-4 z-10">
       <Arrow
         open={curOpen}
         onClick={() => onCurChange(!curOpen)}
         direction={isMobile() ? 'vertical' : 'horizontal'}
       />
+      <a className="cursor-pointer" href="https://github.com/nmsn/pomodoro">
+        <Image src="/github.svg" alt="logo" width={24} height={24} />
+      </a>
     </div>
   );
 };
