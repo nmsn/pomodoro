a976ae6a944862b469b514f44e37792ab37d670a
136696700@qq.com <136696700@qq.com>
2023-10-06 12:39:11 +0800

    feat: calendar 样式及逻辑完善

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,10 +1,21 @@
+import { useMemo, useState } from 'react';
 import classnames from 'classnames';
 import dayjs from 'dayjs';
 
 const W_SUM = 7;
 const H_SUM = 5;
+const WEEK_DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const BLOCKS = new Array(H_SUM)
+  .fill('')
+  .map(() => new Array(W_SUM).fill(''))
+  .flat(1)
+  .fill('');
 
-export const getDaysOfMonth = (year: number, month: number) => {
+export const getDaysOfMonth = (
+  year: number = dayjs().year(),
+  month: number = dayjs().month() + 1,
+) => {
   let firstDayOfMonth = dayjs(`${year}-${month}-1`);
   let lastDayOfMonth = dayjs(`${year}-${month + 1}-1`).subtract(1, 'day');
   // 开始补全第一天前的日期
@@ -24,25 +35,17 @@ export const getDaysOfMonth = (year: number, month: number) => {
     tempDate = tempDate.add(1, 'day');
   }
 
-  return days;
+  return days.map(item => item.format('YYYY-MM-DD'));
 };
 
-const curYear = dayjs().year();
-const curMonth = dayjs().month();
-
-console.log(getDaysOfMonth(curYear, curMonth));
-
-const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const block = new Array(H_SUM)
-  .fill('')
-  .map(() => new Array(W_SUM).fill(''))
-  .flat(1);
-
-const Num = ({ children }: { children: number }) => {
+const Num = ({ children, type = 'fail' }: { children: number; type?: 'success' | 'fail' }) => {
   return (
-    <div className="w-10 h-10 rounded-full bg-red-600 mb-2 mr-2 flex justify-center items-center absolute right-2 bottom-2">
+    <div
+      className={classnames(
+        'w-5 h-5 rounded-full  mb-2 mr-2 flex justify-center items-center font-bold text-sm text-white',
+        type === 'success' ? 'bg-green-500' : 'bg-red-500',
+      )}
+    >
       {children}
     </div>
   );
@@ -56,26 +59,84 @@ const BlockItem = ({
   itemClassName?: string;
 }) => {
   return (
-    <div className={classnames('w-40 h-24 relative  bg-red-200', itemClassName)}>{children}</div>
+    <div className={classnames('w-40 h-24 relative  bg-black p-2', itemClassName)}>{children}</div>
   );
 };
 
+const curYear = dayjs().year();
+const curMonth = dayjs().month() + 1;
+
 const Calendar = () => {
+  const [time, setTime] = useState<[number, number]>([curYear, curMonth]);
+
+  const [year, month] = time;
+
+  const onMonthSwitch = (action: number) => {
+    const day = dayjs(`${year}-${month}`);
+    const newDay = day.add(action, 'month');
+    const result = newDay.format('YYYY-MM').split('-').map(Number) as [number, number];
+    setTime(result);
+  };
+
+  const curDays = useMemo(() => {
+    return getDaysOfMonth(year, month);
+  }, [month, year]);
+
+  const curBlocks = BLOCKS.map((_, index) => {
+    const isCurMonth = curDays[index].includes(`${year}-${month}`);
+    return {
+      date: curDays[index],
+      success: 1,
+      fail: 2,
+      isCurMonth,
+    };
+  });
+
   return (
-    <div className={classnames('h-full w-full flex justify-center items-center')}>
-      <div className="grid grid-rows-6 grid-cols-7 gap-1">
-        {weekDays.map(item => (
-          <BlockItem key={item}>
-            <div className="w-full h-full flex justify-center items-center">
-              <div>{item}</div>
-            </div>
-          </BlockItem>
-        ))}
-        {block.map(item => (
-          <BlockItem key={item}>
-            <Num>{item}</Num>
-          </BlockItem>
-        ))}
+    <div className={classnames('h-full w-full flex justify-center items-center bg-red-400')}>
+      <div>
+        <div className="w-full h-20 bg-black flex justify-center items-center gap-2 mb-2">
+          <div
+            onClick={() => onMonthSwitch(-1)}
+            className="font-bold text-sm text-red-400 cursor-pointer"
+          >
+            {'<'}
+          </div>
+          <div className="font-bold text-sm text-red-400">{`${year}-${month}`}</div>
+          <div
+            className="font-bold text-sm text-red-400 cursor-pointer"
+            onClick={() => onMonthSwitch(1)}
+          >
+            {'>'}
+          </div>
+        </div>
+        <div className="grid grid-rows-6 grid-cols-7 gap-1">
+          {WEEK_DAYS.map(item => (
+            <BlockItem key={item}>
+              <div className="w-full h-full flex justify-center items-center">
+                <div className="font-bold text-sm text-red-400">{item}</div>
+              </div>
+            </BlockItem>
+          ))}
+          {curBlocks.map(item => (
+            <BlockItem key={item.date}>
+              <div>
+                <div
+                  className={classnames(
+                    'font-bold text-sm text-red-400',
+                    item.isCurMonth ? '' : 'opacity-70',
+                  )}
+                >
+                  {item.date}
+                </div>
+                <div className="flex justify-center items-center absolute right-1 bottom-1">
+                  <Num type="success">{item.success}</Num>
+                  <Num>{item.fail}</Num>
+                </div>
+              </div>
+            </BlockItem>
+          ))}
+        </div>
       </div>
     </div>
   );
