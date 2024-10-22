b674c4ff4f7fce7f535d11b69fe04f9af72fe43e
nmsn <136696700@qq.com>
2024-10-22 23:50:24 +0800

    ✨ feat: 修改按钮样式

--- src/components/Pomodoro/TDPomodoro.tsx
+++ src/components/Pomodoro/TDPomodoro.tsx
@@ -2,35 +2,37 @@ import { clsx } from 'clsx';
 
 import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card';
 
-import Button from '../Button';
-
 import usePomodoro from './usePomodoro';
 
+// TODO 反向类型
+function CircleButton({ children, onClick }: { children: React.ReactNode; onClick?: () => void }) {
+  return (
+    <button onClick={onClick} className="min-w-2 rounded-full bg-black text-white px-2">
+      {children}
+    </button>
+  );
+}
+
 export default function BasePomodoro() {
-  const { onStart, onPause, onReset, onChangeTimeType, timeDisplay, status, timeType, ratio } =
-    usePomodoro();
+  const {
+    onStart,
+    onPause,
+    onReset,
+    onChangeTimeType,
+    timeDisplay,
+    status,
+    ratio /* , timeType */,
+  } = usePomodoro();
 
   return (
     <CardContainer className="inter-var border-black/[0.1]">
       <CardBody className="bg-gray-50  flex flex-col justify-between items-center p-10 w-[300px] h-[300px] rounded-xl border border-black/[0.1]">
         <div className="flex justify-end gap-4 w-full">
           <CardItem translateZ={20}>
-            <Button
-              size="small"
-              type={timeType === 'normal' ? 'inverse' : 'positive'}
-              onClick={() => onChangeTimeType('normal')}
-            >
-              1
-            </Button>
+            <CircleButton onClick={() => onChangeTimeType('normal')}>1</CircleButton>
           </CardItem>
           <CardItem translateZ={20}>
-            <Button
-              size="small"
-              type={timeType === 'double' ? 'inverse' : 'positive'}
-              onClick={() => onChangeTimeType('double')}
-            >
-              2
-            </Button>
+            <CircleButton onClick={() => onChangeTimeType('double')}>2</CircleButton>
           </CardItem>
         </div>
         <CardItem translateZ={50} className="text-7xl font-bold select-none text-center">
@@ -41,6 +43,7 @@ export default function BasePomodoro() {
             <div className="h-[2px] bg-black" style={{ width: ratio * 100 + '%' }} />
           </div>
         </CardItem>
+        {/* TODO 按钮会抖动 */}
         <CardItem
           translateZ={50}
           class={clsx(
@@ -48,17 +51,17 @@ export default function BasePomodoro() {
             status === 'initial' ? 'justify-center' : 'justify-between',
           )}
         >
-          {status === 'initial' && <Button onClick={onStart}>START</Button>}
+          {status === 'initial' && <CircleButton onClick={onStart}>START</CircleButton>}
           {status === 'processing' && (
             <>
-              <Button onClick={onPause}>PAUSE</Button>
-              <Button onClick={onReset}>RESET</Button>
+              <CircleButton onClick={onPause}>PAUSE</CircleButton>
+              <CircleButton onClick={onReset}>RESET</CircleButton>
             </>
           )}
           {status === 'paused' && (
             <>
-              <Button onClick={onStart}>CONTINUE</Button>
-              <Button onClick={onReset}>RESET</Button>
+              <CircleButton onClick={onStart}>CONTINUE</CircleButton>
+              <CircleButton onClick={onReset}>RESET</CircleButton>
             </>
           )}
         </CardItem>
